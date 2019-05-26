f895d98536b918e011b060c13481dad3013915b8
Ferdinand Prantl <prantlf@gmail.com>
2019-05-26 23:07:26 +0200

    Recognize and skip comments during parsing

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,12 @@ module.exports = function(grunt) {
       packageJson: {
         src: [ 'package.json' ]
       },
+      comments: {
+        src: [ 'test/cjson.json' ],
+        options: {
+          ignoreComments: true
+        }
+      },
       overflowTest: {
         src: [ 'test/issue13/**/*.json' ]
       },
@@ -79,7 +85,10 @@ module.exports = function(grunt) {
 
   require('./tasks/jsonlint')(grunt);
 
-  grunt.registerTask('test', [ 'jshint', 'jsonlint:sample', 'jsonlint:packageJson', 'mochaTest' ]);
+  grunt.registerTask('test', [
+    'jshint', 'jsonlint:sample', 'jsonlint:packageJson', 'jsonlint:comments',
+    'mochaTest'
+  ]);
 
   // Default task(s).
   grunt.registerTask('default', [ 'test' ]);
